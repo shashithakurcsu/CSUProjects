eb9f3f05c00997ad76fcca1a515dd44e0c41719d
shashithakurcsu <156634085+shashithakurcsu@users.noreply.github.com>
2024-01-15 17:27:01 -0600

    New code
    New code

---
+++ Module1/part_1.py
@@ -0,0 +1,14 @@
+# This is a Python program to find the addition and subtraction of two numbers.
+# Ask the user to input two numbers (num1 and num2).
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+#Add them together to find the output. 
+num_added = num1 + num2
+#Subtract the two numbers to find the output.
+num_substracted = num1 - num2
+
+# Display the results
+print(f"Addition result: {num1} + {num2} = {num_added}")
+print(f"Subtraction result: {num1} - {num2} = {num_substracted}")
+
+# End of program
---
+++ Module1/part_2.py
@@ -0,0 +1,14 @@
+# This is a Python to find the multiplication and division of two numbers.
+# Ask the user to input two numbers (num1 and num2).
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+#Add them together to find the output. 
+num_multiplied = num1 * num2
+#Subtract the two numbers to find the output.
+num_divided = num1 / num2
+
+# Display the results
+print(f"Multiplication result: {num1} * {num2} = {num_multiplied}")
+print(f"Division result: {num1} / {num2} = {num_divided}")
+
+# End of program
