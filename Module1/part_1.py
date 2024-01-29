8e9da64a029111bd7622ea72df3185cbbcebeb3c
shashithakurcsu <156634085+shashithakurcsu@users.noreply.github.com>
2024-01-29 07:26:29 -0600

    Update part_1.py
    Test

--- Module1/part_1.py
+++ Module1/part_1.py
@@ -6,9 +6,10 @@ print(f"After 12 hours: {(caffeine_mg / 4) :.2f} mg")
 print(f"After 24 hours: {(caffeine_mg / 16) :.2f} mg")
 
 """
-
+import math
 current_price = int(input())
 last_months_price = int(input())
 
 print(f"This house is ${current_price}.  The change is ${current_price - last_months_price} since last month.")
-print(f"The estimated monthly mortgage is ${(current_price * .051)/12 :.2f}")
+print(f"The estimated monthly mortgage is ${(current_price * .051)/12 :.2f}.")
+
