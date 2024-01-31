1a72318ce49a280ac181e8a4fecd1c473d0d51a7
shashithakurcsu <156634085+shashithakurcsu@users.noreply.github.com>
2024-01-30 20:08:00 -0600

    Add files via upload

---
+++ Module3/Assignments/module3_part1.py
@@ -0,0 +1,22 @@
+# Here are given tip and tax percenatage
+tip_percentage = 18
+tax_percentage = 7
+
+# Ask the user to enter the charge for the food
+food_charge = float(input("Enter the charge for the food: $"))
+
+# Calculate the tip and tax
+tip = food_charge * tip_percentage / 100
+tax = food_charge * tax_percentage / 100
+
+# Calculate the total amount
+total_amount = food_charge + tip + tax
+
+# Display the results
+print(f"Original Food Charge: ${food_charge:.2f}")
+print(f"Tip ({tip_percentage}%): ${tip:.2f}")
+print(f"Sales Tax ({tax_percentage}%): ${tax:.2f}")
+print(f"Total Amount: ${total_amount:.2f}")
+
+# Display the results in right aligned
+# We will use string formatting
---
+++ Module3/Assignments/module3_part2.py
@@ -0,0 +1,9 @@
+# Ask the user to enter the current time and the number of hours to wait
+current_time = int(input("Enter the current time (in hours, 24-hour format): "))
+wait_hours = int(input("Enter the number of hours to wait for the alarm : "))
+
+# Calculate the time when the alarm will go off
+alarm_time = (current_time + wait_hours) % 24
+
+# Output the result
+print(f"The alarm will go off at {alarm_time:02d}:00.")
